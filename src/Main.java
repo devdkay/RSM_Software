197783a4e2cbe960052bf312000a8f7b586436c6
devdkay <dev.08dkay@gmail.com>
2023-11-06 09:47:52 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame(); // Creates a Frame
+        frame.setSize(420,420);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
