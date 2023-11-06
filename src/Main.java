bd9da1d8006067c7eed69e6fe1e9b1a6b6867a0b
Manav <manavpatel5703.com>
2023-11-06 10:20:28 -0400

    print

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         frame.setSize(420,420);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        System.out.println("hello");
     }
 }
